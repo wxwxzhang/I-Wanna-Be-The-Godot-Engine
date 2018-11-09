b68ad1753c9d772ccfc4b939d6bcf7081df264e1
761397398@qq.com <35161393+761397398@users.noreply.github.com>
2018-11-09 17:14:22 +0800

    0

--- scripts/world/save.gd
+++ scripts/world/save.gd
@@ -1,21 +1,15 @@
 extends Area2D
 
-var is_player_enter = false
 var can_save = true
 
-func _process(delta):
-	if is_player_enter && Input.is_action_just_pressed("ui_shoot"):
-		_save()
+export(Script) var player
+export(Script) var bullet
 
-func _on_Save_body_entered(body):
-	if body.filename == "res://objects/player/player.tscn":
-		is_player_enter = true
-	if body.filename == "res://objects/player/bullet.tscn":
-		_save()
-func _on_Save_body_exited(body):
-	if body.filename == "res://objects/player.tscn":
-		is_player_enter = false
-	
+func _physics_process(delta):
+	for i in get_overlapping_bodies():
+		if (i is player && Input.is_action_just_pressed("ui_shoot")) || \
+				i is bullet:
+			_save()
 func _save():
 	if can_save:
 		can_save = false
@@ -26,7 +20,6 @@ func _save():
 
 func _on_Timer_timeout():
 	$Sprite.frame = 0
-
 
 func _on_Timer2_timeout():
 	can_save = true
